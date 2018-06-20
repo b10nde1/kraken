64ae76ed59b6cb9d21069528509b36db5a595f1d
Ricky Blondel <rickyblondel@icloud.com>
2018-06-20 19:58:48 +0400

    init

---
+++ popup.js
@@ -0,0 +1,60 @@
+let siteMapFile=document.getElementById('siteMapFile');
+
+siteMapFile.onchange=function(activeTab){
+    let input = event.target;
+    let reader = new FileReader();
+    let searchKey=document.getElementById('searchKey').value;
+    reader.onload = function(){
+        let dataURL = reader.result;
+        document.getElementById("code-to-scan").innerHTML=dataURL;
+        scanDocument(searchKey);
+    };
+    let result = reader.readAsText(input.files[0]);
+}
+
+const scanDocument=(argIn)=> {
+    let listLoc=document.getElementsByTagName('loc');
+    let arg=argIn.split('/*/');
+    let result='';
+    let total=0;
+    let finalList=new Array(arg.length);
+    for(var compt=0;compt<listLoc.length;compt++){
+        let temp=listLoc[compt].innerText;
+        for(var comptArg=0;comptArg<arg.length;comptArg++){
+            let check=temp.search(arg[comptArg]);
+            if(check<0){
+                finalList[total]=arg[comptArg];
+                total+=1;
+                result+='<p>'+temp+'</p>';
+            }
+        }
+    }
+    document.getElementById('mode-sitemap-result-urls').innerHTML=result;
+    copyElement(document.getElementById('result'));
+}
+
+const displayResut=(arg)=>{
+    document.getElementById("result").innerHTML=arg;
+    copyElement(document.getElementById("result"));
+}
+
+const copyElement=(el)=>{
+    let body = document.body, range, sel;
+    if (document.createRange && window.getSelection) {
+        range = document.createRange();
+        sel = window.getSelection();
+        sel.removeAllRanges();
+        try {
+            range.selectNodeContents(el);
+            sel.addRange(range);
+        } catch (e) {
+            range.selectNode(el);
+            sel.addRange(range);
+        }
+    } else if (body.createTextRange) {
+        range = body.createTextRange();
+        range.moveToElementText(el);
+        range.select();
+    }
+    document.execCommand("Copy");
+}
